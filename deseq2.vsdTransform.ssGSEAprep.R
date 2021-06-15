81c2d2694887165510904da4cc1b19181a69b3cc
alexharbison <62221107+alexharbison@users.noreply.github.com>
2021-06-15 13:43:52 -0400

    Create deseq2.vsdTransform.ssGSEAprep.R
    Generates DESeq2 VSD transformed counts and formats for single sample GSEA input. Requires manually adding two lines at the top of file prior to input into ssGSEA program formatted as: "#1.2" [return carriage] "number of genes[tab]number of samples".

---
+++ deseq2.vsdTransform.ssGSEAprep.R
@@ -0,0 +1,32 @@
+# This program can be run as
+#
+# cat counts.txt | Rscript deseq2.vsdTransform.ssGSEAprep.r
+#
+# Produces a table with variance stabilized counts on the standard output.
+
+# Read the command line arguments.
+args = commandArgs(trailingOnly=TRUE)
+
+# Load the library.
+library(DESeq2)
+
+# Read the data from the standard input.
+countData = read.table("stdin", header=TRUE, sep="\t", row.names=1 )
+
+# Build the dataframe from the conditions
+samples = names(countData)
+
+colData = data.frame(samples=samples)
+
+# Create DESEq2 dataset.
+dds = DESeqDataSetFromMatrix(countData=countData, colData=colData, design = ~1)
+
+# Generate variance stabilized normalized matrix and write to file
+vsd <- vst(dds, blind=FALSE)
+vsd.df <- assay(vsd)
+vsd.df.new = data.frame("NAME"=rownames(vsd.df),"DESCRIPTION"="na",vsd.df)
+
+write.table(vsd.df.new, file=paste0(args,"_","vst_matrix_ssGSEA_ready_deseq2.gct"), sep="\t",  row.name=FALSE, col.names=TRUE,quote=FALSE)
+
+
+
